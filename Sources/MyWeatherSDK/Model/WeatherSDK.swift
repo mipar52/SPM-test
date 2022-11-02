d030fd65348d141fbdcdff5c35445ff03239668a
mipar52 <milanparadina97@gmail.com>
2022-11-02 17:14:01 +0100

    Bla5

--- Sources/MyWeatherSDK/Model/WeatherSDK.swift
+++ Sources/MyWeatherSDK/Model/WeatherSDK.swift
@@ -15,6 +15,7 @@ public class MyWeatherSDK {
        
        let frameworkBundle = Bundle(for: HomeViewController.self)
        let homeVC = HomeViewController(nibName: nil, bundle: frameworkBundle)
+       homeVC.modalPresentationStyle = .fullScreen
        
        viewController.modalPresentationStyle = .fullScreen
        viewController.navigationController?.modalPresentationStyle = .fullScreen
