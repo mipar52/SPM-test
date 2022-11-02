da46eb902a462af0677b000d34f534f55138cd3a
mipar52 <milanparadina97@gmail.com>
2022-11-02 17:03:49 +0100

    Testing out

--- Sources/MyWeatherSDK/Model/WeatherSDK.swift
+++ Sources/MyWeatherSDK/Model/WeatherSDK.swift
@@ -15,11 +15,13 @@ public class MyWeatherSDK {
        
        let frameworkBundle = Bundle(for: HomeViewController.self)
        let homeVC = HomeViewController(nibName: nil, bundle: frameworkBundle)
-   
+       homeVC.modalPresentationStyle = .fullScreen
+    
+       
        viewController.modalPresentationStyle = .fullScreen
        viewController.navigationController?.modalPresentationStyle = .fullScreen
        viewController.navigationController?.pushViewController(homeVC, animated: true)
-       viewController.navigationController?.present(homeVC, animated: true)
+       viewController.present(homeVC, animated: true)
        
        
         print("pushing vc")
