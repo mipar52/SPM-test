94911909f5f2c1630f0951b023fb2590509ebbf6
mipar52 <milanparadina97@gmail.com>
2022-11-02 15:35:57 +0100

    Changes

--- Sources/MyWeatherSDK/Model/WeatherSDK.swift
+++ Sources/MyWeatherSDK/Model/WeatherSDK.swift
@@ -14,12 +14,16 @@ public class MyWeatherSDK {
    public func presentWeatherVC(_ viewController: UIViewController) {
 //       let weatherViewController = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(withIdentifier: "weatherVC")
         
+       let sb = UIStoryboard(name: "Main", bundle: Bundle(for: HomeViewController.self))
+       let nextVC = sb.instantiateInitialViewController() as! HomeViewController
+       
        let bundle = UIStoryboard(name: "Main", bundle: Bundle(for: HomeViewController.self))
        let vc = bundle.instantiateInitialViewController()!
        
         viewController.modalPresentationStyle = .fullScreen
-        viewController.navigationController?.pushViewController(vc, animated: true)
-        viewController.present(vc, animated: true)
+        viewController.navigationController?.pushViewController(nextVC, animated: true)
+        viewController.present(nextVC, animated: true)
+       
         print("pushing vc")
 //        if #available(iOS 13.0, *) {
 //            if var topController = UIApplication.shared.keyWindow?.rootViewController  {
