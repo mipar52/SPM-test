eb354ab35a931aaa8e60687122928d48cf2b26d2
mipar52 <milanparadina97@gmail.com>
2022-11-03 15:43:24 +0100

    Added XCFramework

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MyWeatherSDK",
     defaultLocalization: "en",
+    platforms: [.macOS(.v12), .iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -19,11 +20,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MyWeatherSDK",
-            dependencies: []),
-        .testTarget(
-            name: "MyWeatherSDKTests",
-            dependencies: ["MyWeatherSDK"]),
+        .binaryTarget(name: "MyWeatherSDK",
+                      path: "./Sources/MyWeatherSDK.xcframework")
     ]
 )
---
+++ Sources/MyWeatherSDK.xcframework/ios-arm64_x86_64-maccatalyst/MyWeatherSDK.framework/Versions/A/Headers/MyWeatherSDK.h
@@ -0,0 +1,18 @@
+//
+//  MyWeatherSDK.h
+//  MyWeatherSDK
+//
+//  Created by Milan Parađina on 03.11.2022..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyWeatherSDK.
+FOUNDATION_EXPORT double MyWeatherSDKVersionNumber;
+
+//! Project version string for MyWeatherSDK.
+FOUNDATION_EXPORT const unsigned char MyWeatherSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyWeatherSDK/PublicHeader.h>
+
+
